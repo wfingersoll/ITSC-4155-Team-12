75c212e5174262605fb4ba1f80ae89fdb998485c
vcardm <valentinacardm@gmail.com>
2023-03-15 16:05:06 -0400

    Update base.py
    added recommendation function
    - and func to cover string to a list

--- backend/base.py
+++ backend/base.py
@@ -6,10 +6,13 @@ import json
 import pickle 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np 
 
 #load the nlp model and the tfid vect 
-nlp_model = 'models/nlo_model.pkl'
+nlp_model = 'models/nlp_model.pkl'
 #open the file in binary format: read
+clf = pickle.load(open(filename, 'rb'))
+vect = pickle.load(open('transform.pkl', 'rb'))
 
 #function for similairty
 def create_similarity():
@@ -22,6 +25,42 @@ def create_similarity():
     similarity = cosine_similarity(count_matrix)
     return data, similarity 
 
+#recommendation function-> rec
+def rec(m): #movies -> m
+    m = m.lower() #make the movies lowercase 
+    try: #test below for errors/ handles them
+        data.head()
+        similarity.shape
+    except: 
+        data, similarity = create_similarity()
+    #if the movie title is not there, then return sorry :(
+    if m not in data['movie_title'].unique(): 
+        return('The movie you requested is not in our database, sorry!')
+    else: 
+        #now when movie is found
+        i = data.loc[data['movie_title']==m].index[0]
+        m_list = list(enumerate(similarity[i]))
+        m_list = sorted(m_list, key = lambda x:x[1], reverse = True)
+        #the list is ten movies but we exclude the first itme since it is the one being requested
+        m_list = m_list[1:11]
+        #create empty array to add the movie titles 
+        l = []
+        for i in range(len(list)):
+            u =m_list[i][0]
+            l.append(data['movie_title'][u])
+        return l 
+
+#convert list of string to just a list -> ["abc", "def"]
+def convert_to_list(my_list):
+    my_list = my_list.split(' ","') 
+    #replacing the brackets []
+    my_list[0] = my_list[0].replace('["', '')
+    my_list[-1] = my_list[-1].replace('"]', '')
+    return my_list
+
+# def get_suggestions():
+#     datat = pd.read_csv("final_data/new_moviedata.csv")
+#     return list(data['movie_title'].str.capitalize()) #return movies with titles capitalized 
 
 api = Flask(__name__)
 
