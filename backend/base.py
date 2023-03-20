69f872294765d94f0936ac88da68cb1fd3f2d0c6
Will Ingersoll <wingerso@uncc.edu>
2023-03-20 08:49:53 -0400

    Added first version of pageable api

--- backend/base.py
+++ backend/base.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask import request
 import pandas as pd
-
-from utils.string_transforms import string_to_list
-from recommendation import get_other_movies_based_on_content
-
 import json
 import pickle 
 from sklearn.feature_extraction.text import CountVectorizer
@@ -14,28 +10,23 @@ import numpy as np
 #load the nlp model and the tfid vect 
 nlp_model = 'models/nlp_model.pkl'
 #open the file in binary format: read
-clf = pickle.load(open(filename, 'rb'))
-vect = pickle.load(open('transform.pkl', 'rb'))
+clf = pickle.load(open(nlp_model, 'rb'))
+vect = pickle.load(open('models/transform.pkl', 'rb'))
 
 #function for similairty
-def create_similarity():
-    data = pd.read_csv("final_data/new_moviedata.csv")
+def create_similarity(data):
     #create the count matrix as cv
     cv = CountVectorizer()
     #create the matrix based on the column comb which is the combination of them all 
-    count_matrix = cv.fit_transform(data['comb'])
+    count_matrix = cv.fit_transform(data['all'].values.astype('U'))
     #create the similiaity score matrix 
     similarity = cosine_similarity(count_matrix)
     return data, similarity 
 
 #recommendation function-> rec
-def rec(m): #movies -> m
+def rec(m, data): #movies -> m
     m = m.lower() #make the movies lowercase 
-    try: #test below for errors/ handles them
-        data.head()
-        similarity.shape
-    except: 
-        data, similarity = create_similarity()
+    data, similarity = create_similarity(data)
     #if the movie title is not there, then return sorry :(
     if m not in data['movie_title'].unique(): 
         return('The movie you requested is not in our database, sorry!')
@@ -48,7 +39,7 @@ def rec(m): #movies -> m
         m_list = m_list[1:11]
         #create empty array to add the movie titles 
         l = []
-        for i in range(len(list)):
+        for i in range(len(m_list)):
             u =m_list[i][0]
             l.append(data['movie_title'][u])
         return l 
@@ -86,7 +77,7 @@ def search_prod_info():
             "genres": [],
         },
         'movie_score': {
-            'year': []
+            'release': []
         }
     }
     
@@ -96,30 +87,45 @@ def search_prod_info():
             for key in response_body['movie_data']:
                 response_body['movie_data'][key].extend(data[key].to_list())
 
-    if response_body['movie_data']['movie_title']:
-        for title in movie_score.loc[:, 'title']:
-            if(title.strip().lower()=='eraserhead'):
-                print('hi')
-            if(title.strip().lower()==response_body['movie_data']['movie_title'][0]):
-                row = movie_score.loc[movie_score['title']==title]
-                response_body['movie_score']['year'] = row['year'].values[0]
 
-    for section in response_body:
-        for entry in response_body[section]:
-            if not response_body[section][entry]:
-                response_body[section][entry] = ['Data Not Available']
+    movie_info_extra = pd.read_csv('data/movies_metadata.csv')
+    release_date = movie_info_extra.loc[movie_info_extra['original_title'].astype(str).str.lower() == response_body['movie_data']['movie_title'][0]]
+   
 
-    index = movie_score.index[movie_score['title'].strip().lower() == response_body['movie_data']['movie_title']].tolist()
-    print(index)
-    content = get_other_movies_based_on_content()
+    similar_films = rec(response_body['movie_data']['movie_title'][0], movie_data)
 
     final_body = json.dumps({
-        'title': response_body['movie_data']['movie_title'][0].capitalize(),
-        'director': response_body['movie_data']['director_name'][0].capitalize(),
+        'title': response_body['movie_data']['movie_title'][0],
+        'director': response_body['movie_data']['director_name'][0],
         'genres': response_body['movie_data']['genres'],
-        'year': response_body['movie_score']['year']
+        'year': release_date['release_date'].values[0],
+        'similar': similar_films
     })
 
-    print(final_body)
-
     return final_body
+
+@api.route('/get-page')
+def get_page():
+    
+    page_num = request.args.get('page_num', default=1, type=int)
+    page_length = request.args.get('page_length', default=18, type=int)
+
+    movie_data = pd.read_csv("final_data/new_moviedata.csv")
+
+    total_pages = len(movie_data)/page_length
+
+    #Make sure page num is within the max pages
+    if(page_num>total_pages):
+        page_num = int(total_pages-1)
+
+    start_index = page_length*page_num
+    end_index = (page_length*page_num)+page_length
+
+    titles = movie_data['movie_title'].values.tolist()
+    page_titles = titles[start_index:end_index]
+
+    response_body = json.dumps({
+        'titles': page_titles
+    })
+
+    return response_body
