6b344b5c5cbec7971e14ba80f6ad64f3dbddfe1d
Will Ingersoll <wingerso@uncc.edu>
2023-03-24 10:27:18 -0400

    Added better frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,100 +1,21 @@
-import { useState } from 'react'
-import axios from "axios";
-import './bootstrap.darkly.css';
-
+import React from 'react';
+import './App.css';
+import Navbar from './components/index';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages/home';
+import Search from "./pages/search";
+  
 function App() {
-    // new line start
-    const [query, setQuery] = useState('')
-    const [movieData, setMovieData] = useState(null)
-
-    const getData = event => {
-        event.preventDefault();
-        axios({
-            method: "GET",
-            url: "/search-prod-info?query=" + query,
-        }).then(response => {
-            const res = response.data;
-            setMovieData(({
-                movie_title: res.title,
-                director_name: res.director,
-                genres: res.genres,
-                title_year: res.year
-            }))
-        })
-    }
-    //end of new line
-
-    return (
-        <div className="row">
-            <h1 className="mb-5">MovieDB</h1>
-
-            {/*
-                Input box.
-                TODO: MAKE THIS A SEPARATE COMPONENT
-            */}
-            <div className="col-lg-6 mb-5">
-                <form onSubmit={getData}>
-                    <div className="mb-3">
-                        <label htmlFor="query-input-id" className="form-label">Enter Query:</label>
-                        <input type="text"
-                               className="form-control"
-                               id="query-input-id"
-                               value={query}
-                               onChange={e => setQuery(e.target.value)}
-                        />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
-            </div>
-
-            {movieData &&
-                <div className="col-lg-6">
-                    <h5>{movieData.movie_title}</h5>
-                    <table className="table">
-                        <tbody>
-                            <tr>
-                                <th scope="row">Director</th>
-                                <td>{movieData.director_name}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Genres</th>
-                                <td>{movieData.genres}</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">Release Date</th>
-                                <td>{movieData.title_year}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            }
-        </div>
-    )
-
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       {/* new line start*/}
-  //       <form onSubmit={getData}>
-  //         <label>Enter Query: <span/>
-  //           <input
-  //           type="text"
-  //           value={query}
-  //           onChange={(e) => setQuery(e.target.value)}
-  //           />
-  //         </label>
-  //         <input type="submit" />
-  //       </form>
-  //       {movieData && <div>
-  //             <p>Movie Title: {movieData.movie_title}</p>
-  //             <p>Director: {movieData.director_name}</p>
-  //             <p>Release Date: {movieData.title_year}</p>
-  //           </div>
-  //       }
-  //        {/* end of new line */}
-  //     </header>
-  //   </div>
-  // );
+return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route exact path='/home' element={<Home />} />
+        <Route path='/search' element={<Search/>} />
+    </Routes>
+    </Router>
+);
 }
-
+  
 export default App;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Nav, NavLink, NavMenu }
+	from "./NavbarElements";
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<NavMenu>
+		<NavLink to="/home" activeStyle>
+			Home
+		</NavLink>
+		<NavLink to="/search" activeStyle>
+			Search
+		</NavLink>
+		</NavMenu>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
